1bd0e84aeb53000785dc404e5cea280ef2fe31a7
Telman Alakbarov <67955826+telmanalakbarov@users.noreply.github.com>
2021-02-27 19:20:57 +0400

    Update and rename mesele19.java to mesele2_19.java

--- mesele19.java
+++
@@ -1,8 +0,0 @@
-public class mesele19 {
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.printf("*%n**%n***%n****%n*****%n");
-
-    }
-    
-}
---
+++ mesele2_19.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meseleler.pkg19;
+
+/**
+ *
+ * @author 99470
+ */
+public class mesele219 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("*%n**%n***%n****%n*****%n");
+        // TODO code application logic here
+    }
+    
+}
