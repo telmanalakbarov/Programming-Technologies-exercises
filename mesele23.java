4eff0db8af4f0407c79823ea9319153a9f615117
Telman Alakbarov <67955826+telmanalakbarov@users.noreply.github.com>
2021-02-27 19:24:33 +0400

    Update mesele23.java

--- mesele23.java
+++ mesele23.java
@@ -1,8 +1,22 @@
-public class mesele23 {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meseleler.pkg23;
 
+/**
+ *
+ * @author 99470
+ */
+public class mesele2_23 {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+        // TODO code application logic here
     }
     
 }
