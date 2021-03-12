0387ca0fa92fbd5451fcaeaa38711577bb151e6e
Telman Alakbarov <67955826+telmanalakbarov@users.noreply.github.com>
2021-03-12 13:14:49 +0400

    Add files via upload

---
+++ mesele2_29.java
@@ -0,0 +1,14 @@
+public class mesele2_29 {
+    public static void main(String[] args){
+            char[] cArr = {'A', 'B', 'C', 'a', 'b', 'c', '0', '1', '2', '$', '+', '/', ' '};
+    
+            for(char c : cArr){
+                printResults(c);
+            }
+        }
+        // print results
+        private static void printResults(char c){
+            System.out.printf("The character %c has the value of %d\n", c, ((int) c));
+        }
+    }
+    
